8f9274505642e000e1f6bbbdc6b58c4e3df68bd9
Vijay_9524 <omvijay44>
2022-01-06 21:33:30 -0800

    first commit

---
+++ src/main/java/com/vijay/demo/ExamServerRepoApplication.java
@@ -0,0 +1,13 @@
+package com.vijay.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamServerRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamServerRepoApplication.class, args);
+	}
+
+}
